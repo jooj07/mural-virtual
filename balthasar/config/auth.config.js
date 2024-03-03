39e08f0ca9348958f34bd353fe383e36ece89ecc
Joao Vitor <65420757+jooj07@users.noreply.github.com>
2024-03-03 19:07:37 -0400

    limpa auth.config

--- balthasar/config/auth.config.js
+++ balthasar/config/auth.config.js
@@ -2,10 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   chaveSecreta: process.env.CHAVE_SECRETA,
-  /* for test */
-  tokenEfemeroValidade: 3600, // 1 minuto - tempo que vai valer o token efêmero
-  jwtValidate: 86400 // 2 minutos - tempo que vai valer o token de sessão
-  // 3600 1 hora
-  // 86400 24 hours
+  tokenEfemeroValidade: 3600, // 1 hora - tempo que vai valer o token efêmero
+  jwtValidate: 86400 // 24h - tempo que vai valer o token do jwt (mas quem 'manda' é o token de sessão)
 
 }
